df6c01c31536a2ec8a93c0f58c47d341a5cd6c8a
vishalHaridas <vishalharidasofficial@gmail.com>
2025-09-01 17:02:02 -0400

    chore: added responsive classes breakpoints

--- llm-chat/tailwind.config.js
+++ llm-chat/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -15,6 +12,13 @@ export default {
       fontFamily: {
         sans: ['ui-sans-serif', 'system-ui', 'Avenir', 'Helvetica', 'Arial'],
       },
+    },
+    screens: {
+      xs: '480px', // Screen type: Mobile devices
+      sm: '640px', // Screen type: Tablets
+      md: '768px', // Screen type: Small laptops
+      lg: '1024px', // Screen type: Desktops
+      xl: '1280px', // Screen type: Large desktops
     },
   },
   plugins: [],
